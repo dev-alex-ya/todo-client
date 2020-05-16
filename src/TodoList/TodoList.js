c0699ff4bc55d9bdd87037d60aeb62d28387d9a6
AlexYa <ya.alexandr.86@gmail.com>
2020-05-16 11:33:23 +0300

    add TodoList component

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -3,16 +3,21 @@ import './TodoList.css';
 import TodoElement from './TodoElement/TodoElement';
 
 const TodoList = (props) => {
-    return(
-        <ul className="todo-list">
-        {
-            
-            props.todoList.map((todo, index) => {
-                return <TodoElement text={todo.text} key={index}/>
-            })
-        }
-        </ul>
-    );
+  return(
+    <ul className="todo-list">
+    {
+      props.todoList.map((todo, index) => {
+        return (
+          <TodoElement
+            text={todo.text}
+            key={index}
+            handleDelete={props.handleDelete.bind(this,index)}
+          />
+        )
+      })
+    }
+    </ul>
+  );
 };
 
 export default TodoList;
